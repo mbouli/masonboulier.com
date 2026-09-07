f48b02b07596df0caeb6defb30b311f3768b83f9
mbouli <masonboulier@gmail.com>
2026-09-07 15:02:36 -0500

    fix: change link to archive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         <nav className="fixed px-9 py-2 top-5 left-0 right-0 z-20 w-full flex justify-between items-center text-lg font-bold text-black">
             <div className="space-x-2 md:space-x-8 text-base md:text-sm text-[#FF3D49]">
                 <StaggeredLink href="/">HOME</StaggeredLink>
-                <StaggeredLink href="https://mase.zip" newTab={true}>ARCHIVE</StaggeredLink>
+                <StaggeredLink href="https://photo.mase.zip" newTab={true}>ARCHIVE</StaggeredLink>
             </div>
             <div className="space-x-4 md:space-x-8 text-base font-bold md:text-sm text-[#FF3D49]">
                 <h1 className="link">TIME (CST) — {currentTime}</h1>
